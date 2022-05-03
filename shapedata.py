614e1fa5532f8cf9d6a41c3d7bd74989da481f35
Andre Ye <73039742+andre-ye@users.noreply.github.com>
2022-05-02 22:48:40 -0700

    Update shapedata.py

--- shapedata.py
+++ shapedata.py
@@ -127,9 +127,12 @@ def make_grid(im, shape=(4, 3), pad_value=100, pad_width=6):
 
     return im
 
-def demo_dataset(data, shape=(4, 3), pad_value=100, pad_width=6, sep_width=3, ood=False):
+def demo_dataset(data, shape=(4, 3), pad_value=100, pad_width=6, sep_width=3, ood=False, color_spec:list=[]):
+    '''
+    ood and color_spec parameters only relevant if using AlecOODShapeData class
+    '''
     if ood:
-        (x1, x1_shapes), (x2, x2_shapes), y = data.create_batch_ood()
+        (x1, x1_shapes), (x2, x2_shapes), y = data.create_batch_ood(color_spec)
     else:
         (x1, x1_shapes), (x2, x2_shapes), y = data.create_batch()
     size = shape[0] * shape[1]
